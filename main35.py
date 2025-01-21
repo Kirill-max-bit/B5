9d069c851ac866f167f9fa9136d0f86ae37e2367
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-01-21 08:31:24 +0000

    сделал задачу

--- main35.py
+++ main35.py
@@ -1,9 +1,11 @@
-def sum_of_squares(n):
-    total = 0
+def alternating_sum(n):
+    S = 0
+    sign = 1  
     for i in range(1, n + 1):
-        total += i * i
-    return total
+        S += sign * (1 / i)
+        sign *= -1  
+    return S
 
-n = int(input("Введите n: "))
-result = sum_of_squares(n)
-print("Сумма квадратов от 1 до", n, "равна:", result)
+n = 10 
+result = alternating_sum(n)
+print(result)
