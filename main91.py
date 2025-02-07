5457c65ad47fe09de982b9cf5c0abfa15d5b4689
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-02-07 10:19:48 +0000

    сделал задачу

--- main91.py
+++ main91.py
@@ -1,18 +1,20 @@
 import math
 
-def task_5_90(n):
+
+def task_5_91(n, x):
     """
-    Вычисляет сумму 1/1! + 1/2! + 1/3! + ... + 1/n!
+    Вычисляет сумму 1 + x¹/1! + x²/2! + x³/3! + ... + xⁿ/n!
     """
     if not 1 <= n <= 10:
         return "n должно быть от 1 до 10"
     
-    result = 0
+    result = 1  # Начинаем с 1
     factorial = 1
+    x_power = 1
     for i in range(1, n + 1):
+        x_power *= x
         factorial *= i
-        result += 1 / factorial
+        result += x_power / factorial
     return result
 
-if __name__ == "__main__":
-    print("Задача 5.90 (n=5):", task_5_90(5))
+print("Задача 5.91 (n=5, x=2):", task_5_91(5, 2))
